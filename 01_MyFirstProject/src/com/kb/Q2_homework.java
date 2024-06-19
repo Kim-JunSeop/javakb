3b739519d27fdac8603eddcac7d515ce601e71c7
JunSeop Kim <wnqtjq8958@naver.com>
2024-06-19 16:31:13 +0900

    0618 실습과제

---
+++ 01_MyFirstProject/src/com/kb/Q2_homework.java
@@ -0,0 +1,22 @@
+package com.kb;
+
+public class Q2_homework {
+
+    public static void main(String[] args) {
+        int age = 30;
+        System.out.println("age = " + age);
+        double height = 175.5;
+        System.out.println("height = " + height);
+        double weight = 70.2;
+        System.out.println("weight = " + weight);
+        String address = "123 Main st, Anytown, USA";
+        System.out.println("address = " + address);
+        String phoneNumber = "010-1234-5678";
+        System.out.println("phoneNumber = " + phoneNumber);
+        String gender = "여성";
+        System.out.println("gender = " + gender);
+        int score = 100;
+        System.out.println("score = " + score);
+
+    }
+}
---
+++ 01_MyFirstProject/src/com/kb/Q3_homework.java
@@ -0,0 +1,22 @@
+package com.kb;
+
+import java.util.Scanner;
+
+public class Q3_homework {
+    public static void main(String[] args) {
+        System.out.println("첫 번째 수학점수를 입력하시오");
+        Scanner sc = new Scanner(System.in);
+        int firstMathScore = sc.nextInt();
+        System.out.println("두 번째 수학점수를 입력하시오");
+        int secondMathScore = sc.nextInt();
+        int mathScore = firstMathScore + secondMathScore;
+
+        System.out.println("첫 번째 영어점수 입력하시오");
+        int firstEnglishScore = sc.nextInt();
+        System.out.println("두 번째 영어점수 입력하시오");
+        int secondEnglishScore = sc.nextInt();
+        int englishScore = firstEnglishScore + secondEnglishScore;
+        System.out.println("mathScore  = " + mathScore + "\nenglishScore =" + englishScore);
+
+    }
+}
